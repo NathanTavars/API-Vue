a3fed466cebc4e1a35fe475ba0e2ef309194c79d
Nathan <nathan48silva@gmail.com>
2023-03-29 17:32:50 -0300

    Finalizado

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import TodosView from '@/views/Todos/TodosView.vue'
+import AddTodo from '@/views/Todos/AddTodo.vue'
+import EditTodo from '@/views/Todos/EditTodo.vue'
 
 const routes = [
   {
@@ -13,6 +15,19 @@ const routes = [
     path: '/todos',
     name: 'todos.index',
     component: TodosView
+  },
+
+  {
+    path: '/todos/create',
+    name: 'todos.create',
+    component: AddTodo
+  },
+
+  {
+    path: '/todos/:id/edit',
+    name: 'todos.edit',
+    component: EditTodo,
+    props: true
   },
   
 ]
--- src/views/Todos/TodosView.vue
+++ src/views/Todos/TodosView.vue
@@ -1,3 +1,56 @@
 <template>
-    <h1>Lista de Tarefas</h1>
+    <h1>
+        Lista de Tarefas 
+        <router-link :to="{name: 'todos.create'}"><i class="fas fa-plus-square"></i></router-link>
+    </h1>
+
+    <div v-show="loading">Carregando as Tarefas </div>
+
+    <ul>
+        <li v-for="todo in todos" :key="todo.identify">
+        <todo-single 
+            :todo="todo" 
+            @todoDeleted="removeTodoList"
+            @todoUpdated="todoUpdated"/></li>
+        
+    </ul>
 </template>
+
+<script>
+
+import { onMounted, ref } from 'vue'
+    import TodoService from '@/services/todos.services'
+    import TodoSingle from './TodoSingle.vue'
+
+    export default {
+            name: 'TodosView',
+            setup() {
+                const todos = ref([])
+
+                const loading = ref(false)
+
+                onMounted(async () => {
+                    loading.value = true
+
+
+                    TodoService.getAll()
+                    .then(response => todos.value = response.data.data)
+                    .catch(error => console.log(error))
+                    .finally(() => loading.value = false)
+                })
+
+                const removeTodoList = (todo) => todos.value.splice(todos.value.indexOf(todo), 1)
+                const todoUpdated = (todo) => todos.value[todos.value.indexOf(todo)] = todo
+
+                return {
+                    todos,
+                    loading,
+                    removeTodoList,
+                    todoUpdated,
+                }
+            },
+            components: {
+                TodoSingle,
+            }
+    }
+</script>
